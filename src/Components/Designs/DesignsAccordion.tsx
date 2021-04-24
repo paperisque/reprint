84f3b5e6cbc54b8745fb406c7db24685be67716d
paperisque <paperisque@gmail.com>
2021-04-24 21:08:19 +0200

    Accord ext...

--- src/Components/Designs/DesignsAccordion.tsx
+++ src/Components/Designs/DesignsAccordion.tsx
@@ -2,30 +2,44 @@ import React, { Key } from "react";
 import { IDesignTreeNode } from "../../global";
 import { IDesignsOverviewProps } from "../../types/designstree";
 
-const treeEbene = ( 
-    ebene: IDesignTreeNode, keys: Key[] ): IDesignTreeNode[] => {
-    
-    const children: IDesignTreeNode[] = [ebene]    
+const treeEbene = (
+    ebene: IDesignTreeNode, keys: Key[]): IDesignTreeNode[] => {
 
-    if ( ebene.children ) {
-        for( let i = 0; i < ebene.children.length; i++ ){
-            if ( keys.indexOf( ebene.children[i].key ) < 0 ){
-                children.push( ...treeEbene( ebene.children[i], keys ) )
+    const children: IDesignTreeNode[] = [ebene]
+
+    if ( keys.indexOf(ebene.key) >= 0 ) return []
+    else if ( ebene.isParent ) keys.push(ebene)
+    else if ( ebene.children ) {
+        for ( let i = 0; i < ebene.children.length; i++) {
+            const next = treeEbene(ebene.children[i], keys)
+            if (next.length) {
+                children.push(...next)
+                break
             }
         }
     }
     return children
-    
+
 }
 
 export default function DesignsAccordion({
-    treeData, expanded, setSelected }: IDesignsOverviewProps){
+    treeData, expanded, setSelected }: IDesignsOverviewProps) {
 
-    const ebenen = [];
+    const ebenen = [], keys: Key[] = []; let branch = []
+
+    for (let j = 0; j < treeData.length; j++) {
+        do {
+            branch = treeEbene(treeData[j], keys)
+            if( branch.length ) ebenen.push(branch)
+
+        } while ( branch.length )
+    }
 
     return (
-        <React.Fragment />
+        <React.Fragment>
+            {JSON.stringify(ebenen)}
+        </React.Fragment>
     )
-    
+
 }
 
